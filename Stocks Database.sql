102d81a8c41b2fe035b60897e0f94a89df5a62b4
jakecarver <54565070+jakecarver@users.noreply.github.com>
2020-04-16 19:26:04 -0700

    Add files via upload

---
+++ Stocks Database.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE game
+
+CREATE TABLE leagues ( 
+
+ column1 league,
+
+  column2 team,
+
+  column3 score1,
+  
+  column4 score2,
+  
+  column5 score3,
+  
+  column6 score4,
+  
+  column7 score5,
+  
+  column8 score6,
+  
+  column9 score7,
+  
+  column10 score8,
+  
+  column11 score9,
+  
+  column12 score10,
+  
+  column13 score11,
+  
+  column14 score12 );
+
+CREATE TABLE users ( 
+
+ column1 league,
+
+  column2 team,
+
+  column3 score,
+  
+  column4 stock1,
+  
+  column5 stock2,
+  
+  column6 stock3,
+  
+  column4 stock4,
+  
+  column4 stock5,
+  
+  column4 stock6,
+  
+  column4 stock7,
+
+  column4 stock8,
+  
+  column4 stock9,
+  
+  column4 stock10 );
+
+CREATE TABLE users ( 
+
+ column1 stockName,
+
+ column1 NASDAQ,
+
+  column3 price,
+
+  column4 weekChange);
