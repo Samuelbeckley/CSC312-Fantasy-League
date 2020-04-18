5dbb6bc5083ddd7d38aa67a5d3a459c04d684343
jwallace01 <60442051+jwallace01@users.noreply.github.com>
2020-04-18 15:19:09 -0400

    Update Stock.js

--- Stock.js
+++ Stock.js
@@ -1,16 +1,19 @@
 class Stock{
     constructor(name, value) {
+        this.name = name;
+        this.value = value;
         console.log("Creating stock.");
     }
     
     getValue(stockAbr){
+        return this.value;
         // request most recent price as held in database
     }
     
     getName(stockAbr){
         //get full name using the abbreviation 
         console.log("Retrieving" + stockAbr + "full name")
-        return "";
+        return this.name;
     }
     
     updateValue(stockAbr){
