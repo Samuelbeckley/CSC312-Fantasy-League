8910816c052afeb3bf43d2dbe40d64cb7d035db7
Bryan Ortiz <31941919+Relaxbry@users.noreply.github.com>
2020-04-29 13:45:56 -0700

    Update Portfolio.js
    Added comments

--- Portfolio.js
+++ Portfolio.js
@@ -1,14 +1,18 @@
+/*Sam and Bryan 
+CSC 312 Software Design
+Code for Portfolio 
+3/14/20
+
+Updates:
+
+Description: 
+
+*/
+
+
 // SAM AND BRYAN
 class Portfolio{
-
-    // constructor(stockName, price, quantity, userCash){
-    //     this.stockName =  stockName;
-    //     this.price =  price;
-    //     this.quantity = quantity;
-    //     this.userCash = userCash;
-    //     this.ownedStocks = new Array(10);
-    // }
-
+    
     constructor(){
         console.log("Portfolio: creating portfolio");
     }
@@ -21,9 +25,8 @@ class Portfolio{
         this.ownedStocks[sLen] = stockName;
         this.ownedStocks[sLen+1] = quantity;
         this.userCash -= (quantity*stockName);
-
       } else {
-        console.log("Portfolio: Not enough cash to buy" + quantity + stockName)
+        console.log("Portfolio: Not enough cash to buy" + quantity + stockName);
       }
     }
 
@@ -40,52 +43,65 @@ class Portfolio{
             }
           }
         }
-
     }
-
-    //check stock price function
-
-
-// getStockData(){
-//     return [this.stockName,this.price,this.quantity];
-// }
-
-
-// get buy(stockName, quantity){
-//     return this.buyStock();
-// }
-
-// buyStock(){
-//     return this.push();
-// }
-
-
-// sell(stockName, quantity){
-//     return this.sellStock();
-// }
-// sellStock(){
-//  return null;
-// }
-
-// balance(userCash){
-//     return this.checkUserBalance();
-// }
-
-// checkUserBalance(){
-// return null;
-// }
-
-// contains(){
-//     return null;
-// }
-
 }
+    
+//The code below is what our intended portfolio was originally going to be but due to time limitations we opted for the above portfolio instead 
+    
+    // constructor(stockName, price, quantity, userCash){
+    //     this.stockName =  stockName;
+    //     this.price =  price;
+    //     this.quantity = quantity;
+    //     this.userCash = userCash;
+    //     this.ownedStocks = new Array(10);
+    // }
+    
+    
+    
+    //This method would have retrieved the information pertaining to the selected stock 
+    // getStockData(){
+    //     return [this.stockName,this.price,this.quantity];
+    // }
+
+    //This method would have allowed the user to buy a stock and selected the desired quantity
+    // get buy(stockName, quantity){
+    //     return this.buyStock();
+    // }
+
+    // buyStock(){
+    //     return this.push();
+    // }
+
+    //This method would have allowed the user to sell stocks and its quantity 
+    // sell(stockName, quantity){
+    //     return this.sellStock();
+    // }
+    // sellStock(){
+    //  return null;
+    // }
+
+    //This method would allow users to check their current balance 
+    // balance(userCash){
+    //     return this.checkUserBalance();
+    // }
+    
+    
+    // checkUserBalance(){
+    // return null;
+    // }
+    
+    //This function would be used to check if the portfolio already contained a purchased stock 
+    // contains(){
+    //     return null;
+    // }
+
+    
 
 
-// var dummyPortfolio = new portfolio("AMZN",2168,5);
+//var dummyPortfolio = new portfolio("AMZN",2168,5);
 
 
-// //if the new added stock is already in the portfolio, only increase stocks quantity otherwise add new stock name, price, and quantity
+//if the new added stock is already in the portfolio, only increase stocks quantity otherwise add new stock name, price, and quantity
 // if (dummyPortfolio.contains("AMZN")){
 //     //increase AMZN quantity
 // }
@@ -99,23 +115,23 @@ class Portfolio{
 // console.log(dummyPortfolio.stockData);
 
 
-// //create a function for transfering stock name data into the parameter stockName
-// //connects alpha vantage to portfolio
-// function stockNameTransfer(alphaVantageStock){
-//     return null;
+//create a function for transfering stock name data into the parameter stockName
+//connects alpha vantage to portfolio
+//function stockNameTransfer(alphaVantageStock){
+// return null;
 // }
 
 
-// //create a function for transfering the stock price data into the parameter price
+//create a function for transfering the stock price data into the parameter price
 // function stockPriceTransfer(alphaVantagePrice){
 //     return null;
 // }
 
-// //create a class for transfering the quantity of stock in our portfolio
+//create a class for transfering the quantity of stock in our portfolio
 // function stockPriceTransfer(alphaVantageQuantity){
 //     return null;
 // }
 
-// //when new stock purchase, add stock name, add stock price, update stock quantity.
+//when new stock purchase, add stock name, add stock price, update stock quantity.
 
-// //if stock sold, remove stock name, remove stock price, update stock quantity.
+ //if stock sold, remove stock name, remove stock price, update stock quantity.
